c6cb8f2e26173c2f0bf175b3323026dc296ebf75
snlcom <admin@adminui-MacBookPro.local>
2021-09-13 17:29:27 +0900

    feat : Upbit api test 중 / crix-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
-import { request } from './api';
+import { api } from './api';
 import { useEffect, useState } from 'react';
 import { Background } from './AppStyled';
+import moment from 'moment';
+
+const _sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
 
 function App() {
   const [markets, setMarkets] = useState<any[]>();
+  const [trends, setTrends] = useState<any[]>();
 
   useEffect(() => {
     const test = async () => {
-      const price = await request.getPriceFromDate('KRW-BTC', '2021-09-05 00:00:00');
-      const allMarkets = await request.getAllMarket();
+      const price = await api.getPriceFromDate('KRW-BTC', '2021-09-05 00:00:00');
+
+      const allMarkets = await api.getAllMarket();
       setMarkets(allMarkets.filter((e: any) => e.market[0] === 'K'));
-      console.log(price, allMarkets);
+
+      const allTrends = await api.getTrendsRate();
+      setTrends(
+        allTrends
+          .filter((e: any) => e.code[11] === 'K')
+          .sort((a: any, b: any) => {
+            if (a.changeRate7Days > b.changeRate7Days) return 1;
+            if (a.changeRate7Days < b.changeRate7Days) return -1;
+            return 0;
+          })
+      );
+
+      console.log(
+        price,
+        allMarkets.filter((e: any) => e.market[0] === 'K'),
+        allTrends
+          .filter((e: any) => e.code[11] === 'K')
+          .sort((a: any, b: any) => {
+            if (a.changeRate7Days > b.changeRate7Days) return 1;
+            if (a.changeRate7Days < b.changeRate7Days) return -1;
+            return 0;
+          })
+          .reverse()
+          .map((e: any) => e.code.slice(11))
+          .slice(0, 10)
+      );
+
+      // for (const e of allMarkets.filter((e: any) => e.market[0] === 'K')) {
+      //   const test = await api.getPriceFromDate(e.market, '2021-09-09 00:00:00');
+      //   await _sleep(300);
+      //   console.log(test);
+      // }
     };
+
+    console.log(moment().subtract(365, 'days').format('YYYY-MM-DD'));
 
     test();
   }, []);
--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_PATH = 'https://api.upbit.com/v1/';
 
-export const request = {
+export const api = {
   getAllMarket: async () => {
     const res = await axios.get(`${API_PATH}market/all?isDetails=true`);
 
@@ -10,6 +10,14 @@ export const request = {
   },
   getPriceFromDate: async (market: string, date: string) => {
     const res = await axios.get(`${API_PATH}candles/days?market=${market}&to=${date}&count=1`);
+
+    return res.data;
+  },
+
+  getTrendsRate: async () => {
+    const res = await axios.get(
+      'https://crix-api-cdn.upbit.com/v1/crix/trends/interval_change_rate'
+    );
 
     return res.data;
   },
